69313e9f413b5c326ba5617d864252b1788a8f49
Marcus Linderoth <marcus@thingsquare.com>
2016-01-19 22:48:33 +0100

    intial commit

---
+++ js/config.js
@@ -0,0 +1,63 @@
+$(function() {
+
+	CMS.init({
+
+		// Name of your site or location of logo file ,relative to root directory (img/logo.png)
+		siteName: 'Bithappens',
+
+		// Tagline for your site
+		siteTagline: 'Another one bit the dust',
+
+		// Email address
+		siteEmail: 'linderoth.marcus@gmail.com',
+
+		// Name
+		siteAuthor: 'Marcus Linderoth',
+
+		// Navigation items
+		siteNavItems: [
+			{ name: 'Github', href: 'https://github.com/msloth', newWindow: true},
+			{ name: 'About'}
+		],
+
+		// Posts folder name
+		postsFolder: 'posts',
+
+		// Homepage posts snippet length
+		postSnippetLength: 120,
+
+		// Pages folder name
+		pagesFolder: 'pages',
+
+		// Site fade speed
+		fadeSpeed: 300,
+
+		// Site footer text
+		footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+		// Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
+		// to Github
+		mode: 'Apache',
+
+		// If Github mode is set, your Github username and repo name. Defaults
+		// to Github pages branch (gh-pages)
+		githubUserSettings: {
+			username: 'msloth',
+			repo: 'msloth.github.io'
+		}
+
+	});
+
+	// Markdown settings
+	marked.setOptions({
+		renderer: new marked.Renderer(),
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: true,
+		smartLists: true,
+		smartypants: false
+	});
+
+});
